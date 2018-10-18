de5711fa7da265e590fc54c139d42d91a584c91d
eggeek <eggeek.me@gmail.com>
2018-10-18 13:57:09 +1100

    add test cases

--- test.cpp
+++ test.cpp
@@ -1,16 +1,45 @@
 #define CATCH_CONFIG_RUNNER
+#include <vector>
 #include "catch.hpp"
+#include "Entry.h"
+#include "cpd.h"
+#include "mapper.h"
+#include "dijkstra.h"
+#include "visualizer.h"
 using namespace std;
 
-unsigned int Factorial( unsigned int number ) {
-    return number <= 1 ? number : Factorial(number-1)*number;
+string mpath;
+int height, width;
+vector<bool> mapData;
+
+TEST_CASE("ObsFree") {
+  ifstream file("test/ObsFree.in");
+  file >> mpath;
+  LoadMap(mpath.c_str(), mapData, width, height);
+  Mapper mapper(mapData, width, height);
+  CPD cpd;
+  AdjGraph g(extract_graph(mapper));
+  Dijkstra dij(g, mapper);
+  int cnth = 0;
+  auto res = dij.run(0);
+  for (auto i: res) if (i & warthog::HMASK) cnth++;
+  REQUIRE(cnth == mapper.node_count() - 1);
 }
 
-TEST_CASE( "Factorials are computed", "[factorial]" ) {
-    REQUIRE( Factorial(1) == 1 );
-    REQUIRE( Factorial(2) == 2 );
-    REQUIRE( Factorial(3) == 6 );
-    REQUIRE( Factorial(10) == 3628800 );
+TEST_CASE("OneObs") {
+  ifstream file("test/OneObs.in");
+  file >> mpath;
+  LoadMap(mpath.c_str(), mapData, width, height);
+  Mapper mapper(mapData, width, height);
+  CPD cpd;
+  AdjGraph g(extract_graph(mapper));
+  Dijkstra dij(g, mapper);
+  int cnth = 0;
+  auto res = dij.run(2);
+  for (auto i: res) if (i & warthog::HMASK) cnth++;
+  vector<string> vis = Visualizer(mapData, mapper).to_strings(2, res);
+  for (string i: vis) cout << i << endl;
+  REQUIRE(cnth == 13);
 }
 
 int main(int argv, char* args[]) {
