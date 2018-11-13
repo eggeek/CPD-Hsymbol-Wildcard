f5a865c8a8f52fab329daeff94144687c9b3ed5f
eggeek <eggeek.me@gmail.com>
2018-11-13 14:36:15 +1100

    add experiment result of new maps

--- shizhe/run_small.sh
+++ shizhe/run_small.sh
@@ -6,13 +6,13 @@ if [ $# -eq 1 ]
     t="-run"
 fi
 
-for ((h=0; h<=2; h++)) {
-  for i in `ls maps/dao/*.map`; do
+for i in `ls maps/*.map`; do
+  for ((h=0; h<=2; h++)) {
     name=$(basename "${i%.*}")
-    mpath="maps/dao/${name}.map"
-    spath="scens/dao/${name}.map.scen"
+    mpath="maps/${name}.map"
+    spath="scens/${name}.map.scen"
     cmd="./bin/main $t $h ${mpath} ${spath}"
     echo $cmd
     eval $cmd
-  done
-}
+  }
+done
