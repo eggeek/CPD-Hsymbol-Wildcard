0a0fcb7af297fa3cbefe2c53aa75123d8278eb4b
eggeek <eggeek.me@gmail.com>
2018-11-20 01:26:29 +1100

    update run scripts

--- integrated-sz/run_small.sh
+++ integrated-sz/run_small.sh
@@ -6,13 +6,29 @@ if [ $# -eq 1 ]
     t="-run"
 fi
 
-for i in `ls small_maps/*.map`; do
-  for ((h=0; h<=3; h++)) {
-    name=$(basename "${i%.*}")
-    mpath="small_maps/${name}.map"
-    spath="scens/${name}.map.scen"
-    cmd="./bin/main $t $h ${mpath} ${spath}"
-    echo $cmd
-    eval $cmd
-  }
+
+for i in `ls -rS small_maps/*.map`; do
+  name=$(basename "${i%.*}")
+  mpath="small_maps/${name}.map"
+  spath="scens/${name}.map.scen"
+
+  # run l3 + pure
+  cmd="./bin/main $t -pure 3 ${mpath} ${spath}"
+  echo $cmd
+  eval $cmd
+
+  # run l3 + wildcard 
+  cmd="./bin/main $t 3 ${mpath} ${spath}"
+  echo $cmd
+  eval $cmd
+
+  # run wildcard + l0
+  cmd="./bin/main $t 0 ${mpath} ${spath}"
+  echo $cmd
+  eval $cmd
+
+  # run pure
+  cmd="./bin/main $t -pure 0 ${mpath} ${spath}"
+  echo $cmd
+  eval $cmd
 done
