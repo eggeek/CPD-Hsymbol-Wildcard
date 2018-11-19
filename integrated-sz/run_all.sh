a5d429c20a461fc5925c48b1be2dce382134d291
eggeek <eggeek.me@gmail.com>
2018-11-20 02:41:07 +1100

    update run script

--- integrated-sz/run_all.sh
+++ integrated-sz/run_all.sh
@@ -1,20 +1,34 @@
-
-#!/bin/sh
+#!/bin/bash
 if [ $# -eq 1 ]
   then
     t=$1
   else
-    t="-full"
+    t="-run"
 fi
 
-for ((h=0; h<=3; h++)) {
-  for i in `ls maps/dao/*.map`; do
-    name=$(basename "${i%.*}")
-    mpath="maps/dao/${name}.map"
-    spath="scens/dao/${name}.map.scen"
-    #cmd="./bin/main $t $h ${mpath} ${spath}"
-    cmd="./src_pwc_cut_hsymb $t $h ${mpath} ${spath}"
-    echo $cmd
-    eval $cmd
-  done
-}
+
+for i in `ls -rS maps/*.map`; do
+  name=$(basename "${i%.*}")
+  mpath="small_maps/${name}.map"
+  spath="scens/${name}.map.scen"
+
+  # run l3 + pure
+  cmd="./bin/main $t -pure 3 ${mpath} ${spath}"
+  echo $cmd
+  eval $cmd
+
+  # run l3 + wildcard 
+  cmd="./bin/main $t 3 ${mpath} ${spath}"
+  echo $cmd
+  eval $cmd
+
+  # run wildcard + l0
+  cmd="./bin/main $t 0 ${mpath} ${spath}"
+  echo $cmd
+  eval $cmd
+
+  # run pure
+  cmd="./bin/main $t -pure 0 ${mpath} ${spath}"
+  echo $cmd
+  eval $cmd
+done
