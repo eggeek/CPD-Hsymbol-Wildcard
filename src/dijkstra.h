aff5ea76c27b6337969077210695fd2b6171c546
eggeek <eggeek.me@gmail.com>
2019-06-07 13:40:35 +1000

    fix format

--- src/dijkstra.h
+++ src/dijkstra.h
@@ -86,7 +86,7 @@ public:
     return directions[t];
   }
 
-    const std::vector<unsigned short>&run(int source_node, int hLevel, vector<int>&square_side){
+  const std::vector<unsigned short>&run(int source_node, int hLevel, vector<int>&square_side){
 
         allowed = run(source_node, hLevel);
         int side = computeMaxSquare(mapper, mapper.operator()(source_node));
@@ -103,92 +103,90 @@ private:
   vector<int> directions;
 
 
-    int computeMaxSquare(Mapper mapper, xyLoc loc)
-    {
-	const int16_t* dxv = warthog::dx;
+  int computeMaxSquare(Mapper mapper, xyLoc loc) {
+	  const int16_t* dxv = warthog::dx;
   	const int16_t* dyv = warthog::dy;
-        int x = loc.x;
-        int y = loc.y;
-        if(mapper.operator()(loc) == -1)
-        {
-            bool tryToGetObstacle = false;
-        }
-        int side = 1;
-        int node_count = 1;
-        bool expandSquare = true;
+      int x = loc.x;
+      int y = loc.y;
+      if(mapper.operator()(loc) == -1)
+      {
+          bool tryToGetObstacle = false;
+      }
+      int side = 1;
+      int node_count = 1;
+      bool expandSquare = true;
 
-        auto is_valid = [&](int dx, int dy)
-        {
-            xyLoc loc1;
-            loc1.x = x+dx;
-            loc1.y = y+dy;
-            if((x+dx >= 0)&&(x+dx < mapper.width())&&(y+dy >= 0)&&(y+dy < mapper.height())&&(mapper.operator()(loc1) != -1))
-            {
-                for(int j=0; j<8; j++)
-                {
-                    if((allowed[mapper.operator()(loc1)]&(1 << j)) != 0)
-                    {
-                        xyLoc d;
-			d.x = loc.x;
-			d.y = loc.y;
-			d.x += dxv[j];
-      			d.y += dyv[j];
-                        if(is_nat(loc, d, loc1))
-                        {
-                            return 1;
-                        }
-                    }
-                }
-                return -1;
-            }
-            else
-            {
+      auto is_valid = [&](int dx, int dy)
+      {
+          xyLoc loc1;
+          loc1.x = x+dx;
+          loc1.y = y+dy;
+          if((x+dx >= 0)&&(x+dx < mapper.width())&&(y+dy >= 0)&&(y+dy < mapper.height())&&(mapper.operator()(loc1) != -1))
+          {
+              for(int j=0; j<8; j++)
+              {
+                  if((allowed[mapper.operator()(loc1)]&(1 << j)) != 0)
+                  {
+                      xyLoc d;
+                      d.x = loc.x;
+                      d.y = loc.y;
+                      d.x += dxv[j];
+                      d.y += dyv[j];
+                      if(is_nat(loc, d, loc1))
+                      {
+                          return 1;
+                      }
+                  }
+              }
+              return -1;
+          }
+          else
+          {
+              return 0;
+          }
+      };
 
-                return 0;
-            }
-        };
-
-        while(expandSquare)
-        {
-            int nodes_visited = 0;
-            side += 2;
-            for(int dy = -(side-1)/2; dy <= (side-1)/2; dy++)
-            {
-                int dx = (side-1)/2;
-                if((is_valid(-dx, dy) == -1)||(is_valid(dx, dy) == -1))
-                {
-                    return side-2;
-                }
-                else
-                {
-                    nodes_visited += is_valid(-dx, dy);
-                    nodes_visited += is_valid(dx, dy);
-                }
-            }
-            for(int dx = -(side-1)/2 + 1; dx<= (side-1)/2 -1; dx++)
-            {
-                int dy = (side-1)/2;
-                if((is_valid(dx, -dy) == -1)||(is_valid(dx, dy) == -1))
-                {
-                    return side-2;
-                }
-                else
-                {
-                    nodes_visited += is_valid(dx, -dy);
-                    nodes_visited += is_valid(dx, dy);
-                }
-            }
-            if(nodes_visited == 0)
-            {
-                return side-2;
-            }
-            else {
-                node_count += nodes_visited;
-                if (node_count == mapper.node_count()) {
-                    return side;
-                }
-            }
-        }
+      while(expandSquare)
+      {
+          int nodes_visited = 0;
+          side += 2;
+          for(int dy = -(side-1)/2; dy <= (side-1)/2; dy++)
+          {
+              int dx = (side-1)/2;
+              if((is_valid(-dx, dy) == -1)||(is_valid(dx, dy) == -1))
+              {
+                  return side-2;
+              }
+              else
+              {
+                  nodes_visited += is_valid(-dx, dy);
+                  nodes_visited += is_valid(dx, dy);
+              }
+          }
+          for(int dx = -(side-1)/2 + 1; dx<= (side-1)/2 -1; dx++)
+          {
+              int dy = (side-1)/2;
+              if((is_valid(dx, -dy) == -1)||(is_valid(dx, dy) == -1))
+              {
+                  return side-2;
+              }
+              else
+              {
+                  nodes_visited += is_valid(dx, -dy);
+                  nodes_visited += is_valid(dx, dy);
+              }
+          }
+          if(nodes_visited == 0)
+          {
+              return side-2;
+          }
+          else {
+              node_count += nodes_visited;
+              if (node_count == mapper.node_count()) {
+                  return side;
+              }
+          }
+      }
     }
 
     bool is_nat(xyLoc source, xyLoc target, xyLoc node)
