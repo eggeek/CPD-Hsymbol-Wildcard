7b1d4d889ff9ede0b2db7858bdd234d78cd43e7d
eggeek <eggeek.me@gmail.com>
2019-07-18 16:02:34 +1000

    fix centroid computing

--- pre.sh
+++ pre.sh
@@ -1,16 +1,16 @@
 #!/bin/sh
 
 ./bin/main -P -M maps/arena.map -L 3
-./bin/main -P -M maps/arena.map -L 3 --order SPLIT
+./bin/main -P -M maps/arena.map -L 3 --centroid
 ./bin/main -P -M maps/arena.map -L 3 --order CUT
 ./bin/main -P -M maps/arena.map -L 3 --order CUT --centroid
 
 ./bin/main -P -M maps/brc000d.map -L 3
-./bin/main -P -M maps/brc000d.map -L 3 --order SPLIT
+./bin/main -P -M maps/brc000d.map -L 3 --centroid
 ./bin/main -P -M maps/brc000d.map -L 3 --order CUT
 ./bin/main -P -M maps/brc000d.map -L 3 --order CUT --centroid
 
 ./bin/main -P -M maps/dao/brc101d.map -L 3
-./bin/main -P -M maps/dao/brc101d.map -L 3 --order SPLIT
+./bin/main -P -M maps/dao/brc101d.map -L 3 --centroid
 ./bin/main -P -M maps/dao/brc101d.map -L 3 --order CUT
 ./bin/main -P -M maps/dao/brc101d.map -L 3 --order CUT --centroid
--- src/centroid.h
+++ src/centroid.h
@@ -17,7 +17,8 @@ int find_square_centroid(int h0, int h1, int w0, int w1, const Mapper& mapper) {
   return -1;
 }
 
-void bfs(int s, int r, vector<int>& fa, vector<bool>& vis, const Mapper& mapper) {
+void bfs(int s, int r, vector<int>& fa, vector<bool>& vis, const Mapper& mapper,
+    int x0, int x1, int y0, int y1) {
   priority_queue<pair<double, int>, vector<pair<double, int>>, greater<pair<double, int>>>q;
   vector<double> dist(mapper.node_count(), 1e10);
   q.push({0, s});
@@ -34,8 +35,15 @@ void bfs(int s, int r, vector<int>& fa, vector<bool>& vis, const Mapper& mapper)
       int nxty = mapper(c.second).y + warthog::dy[move];
       pair<double, int> nxt = {c.first + warthog::doublew[move], mapper(xyLoc{(int16_t)nxtx, (int16_t)nxty})};
       dist[nxt.first] = min(dist[nxt.second], nxt.first);
-      if (nxt.first <= (double)r && !vis[nxt.second]) {
-        q.push(nxt);
+      if (x0 == -1) {
+        if (nxt.first <= (double)r/2 && !vis[nxt.second]) {
+          q.push(nxt);
+        }
+      }
+      else if (nxtx >= x0 && nxtx <= x1 && nxty >= y0 && nxty <= y1) {
+        if (!vis[nxt.second]) {
+          q.push(nxt);
+        }
       }
     }
   }
@@ -46,22 +54,21 @@ vector<int> compute_centroid(Mapper& mapper, int r) {
   int width = mapper.width();
   vector<int> centroids;
   vector<int> fa(mapper.node_count(), -1);
+  vector<bool> vis(mapper.node_count(), false);
   for (int h=0; h<height; h += r)
   for (int w=0; w<width;  w += r) {
     int h0 = h, h1 = min(h+r-1, height-1);
     int w0 = w, w1 = min(w+r-1, width-1);
     int c = find_square_centroid(h0, h1, w0, w1, mapper);
-    if (c != -1)
+    if (c != -1) {
       centroids.push_back(c);
+      bfs(c, r, fa, vis, mapper, w0, w1, h0, h1);
+    }
   }
 
-  vector<bool> vis(mapper.node_count(), false);
-  for (int i: centroids) {
-    bfs(i, r, fa, vis, mapper);
-  }
   for (int i=0; i<mapper.node_count(); i++) if (!vis[i]) {
     centroids.push_back(i);
-    bfs(i, r, fa, vis, mapper);
+    bfs(i, r, fa, vis, mapper, -1, -1, -1, -1);
   }
   sort(centroids.begin(), centroids.end());
   mapper.set_centroids(fa);
