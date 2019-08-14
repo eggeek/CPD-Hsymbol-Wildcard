8b1b38d99979f6b2df4613f43ba9ab17dffce297
eggeek <eggeek.me@gmail.com>
2019-08-14 13:24:48 +1000

    update scripts

--- sub-pre.sh
+++ sub-pre.sh
@@ -12,30 +12,58 @@ map_dir="./maps/gppc/"
 scen_dir="./scens/gppc/"
 order="DFS"
 
-echo "----- Preprocess CPD -----" 
-# precompute cpd
-for i in "${map_names[@]}"; do
-  mpath="${map_dir}${i}.map"
-  spath="${scen_dir}${i}.map.scen"
-  for c in `seq 0 2 8`; do
-    cmd="./bin/main -P -M ${mpath} -L 3 --centroid ${c}"
+run_cpd() {
+  echo "----- Preprocess CPD -----" 
+  # precompute cpd
+  for i in "${map_names[@]}"; do
+    mpath="${map_dir}${i}.map"
+    spath="${scen_dir}${i}.map.scen"
+    for c in `seq 0 2 8`; do
+      cmd="./bin/main -P -M ${mpath} -L 3 --centroid ${c}"
+      echo $cmd
+      eval $cmd
+    done
+  done
+}
+
+run_tree() {
+  echo "----- Preprocess competitor: tree -----" 
+  # precompute tree
+  cd ./competitors/tree
+
+  for i in "${map_names[@]}"; do
+    mpath="${map_dir}${i}.map"
+    spath="${scen_dir}${i}.map.scen"
+
+    cmd="./test -pre ${mpath} ${spath}"
     echo $cmd
     eval $cmd
   done
+
+  cd -
+}
+
+run() {
+  case "$1" in 
+    cpd) run_cpd ;;
+    tree) run_tree ;;
+    all)
+      run_cpd
+      run_tree;;
+    *) 
+      run_cpd
+      run_tree;;
+  esac
+}
+
+run $1
+if [ "$1" != "" ]
+then
+  shift
+fi
+while [ "$1" != "" ]; do
+  run $1
+  shift
 done
 
-echo "----- Preprocess competitor: tree -----" 
-# precompute tree
-cd ./competitors/tree
-
-for i in "${map_names[@]}"; do
-  mpath="${map_dir}${i}.map"
-  spath="${scen_dir}${i}.map.scen"
-
-  cmd="./test -pre ${mpath} ${spath}"
-  echo $cmd
-  eval $cmd
-done
-
-cd -
 echo "Done."
--- sub-run.sh
+++ sub-run.sh
@@ -1,11 +1,11 @@
-#!/usr/bin/env bash
+#!/bin/bash
 map_names=(
-#Archipelago
+Archipelago
 ca_cavern1_haunted
 maze-100-1
-#orz300d
-#random-100-33
-#room-100-10
+orz300d
+random-100-33
+room-100-10
 )
 
 map_dir="./maps/gppc/"
@@ -14,60 +14,93 @@ out_dir="./outputs/cpd/"
 order="DFS"
 
 
-echo "***** run CPD *****"
-# run cpd
-for i in "${map_names[@]}"; do
-  mpath="${map_dir}${i}.map"
-  spath="${scen_dir}${i}.map.scen"
+run_cpd() {
+  echo "----- run CPD -----"
+  # run cpd
+  for i in "${map_names[@]}"; do
+    mpath="${map_dir}${i}.map"
+    spath="${scen_dir}${i}.map.scen"
 
-  indexpath="./index_data/${i}.map-DFS-3-opt"
-  cmd="./bin/main -R -M ${mpath} -S ${spath} -I ${indexpath} -O ${out_dir}${i}-3-dfs-opt.txt"
-  echo $cmd
-  eval $cmd
-
-  indexpath="./index_data/${i}.map-DFS-3-opt-inv"
-  cmd="./bin/main -R -M ${mpath} -S ${spath} -I ${indexpath} -O ${out_dir}${i}-3-dfs-opt-inv.txt"
-  echo $cmd
-  eval $cmd
-
-  for c in `seq 2 2 8`; do
-    indexpath="./index_data/${i}.map-DFS-3-c${c}"
-    cmd="./bin/main -R -M ${mpath} -S ${spath} -I ${indexpath} -O ${out_dir}${i}-3-dfs-c${c}.txt"
+    indexpath="./index_data/${i}.map-DFS-3-opt"
+    cmd="./bin/main -R -M ${mpath} -S ${spath} -I ${indexpath} -O ${out_dir}${i}-3-dfs-opt.txt"
     echo $cmd
     eval $cmd
 
-    indexpath="./index_data/${i}.map-DFS-3-c${c}-inv"
-    cmd="./bin/main -R -M ${mpath} -S ${spath} -I ${indexpath} -O ${out_dir}${i}-3-dfs-c${c}-inv.txt"
+    indexpath="./index_data/${i}.map-DFS-3-opt-inv"
+    cmd="./bin/main -R -M ${mpath} -S ${spath} -I ${indexpath} -O ${out_dir}${i}-3-dfs-opt-inv.txt"
+    echo $cmd
+    eval $cmd
+
+    for c in `seq 2 2 8`; do
+      indexpath="./index_data/${i}.map-DFS-3-c${c}"
+      cmd="./bin/main -R -M ${mpath} -S ${spath} -I ${indexpath} -O ${out_dir}${i}-3-dfs-c${c}.txt"
+      echo $cmd
+      eval $cmd
+
+      indexpath="./index_data/${i}.map-DFS-3-c${c}-inv"
+      cmd="./bin/main -R -M ${mpath} -S ${spath} -I ${indexpath} -O ${out_dir}${i}-3-dfs-c${c}-inv.txt"
+      echo $cmd
+      eval $cmd
+    done
+  done
+}
+
+run_tree() {
+  echo "----- run competitor: tree -----"
+  # run tree
+  cd ./competitors/tree
+
+  for i in "${map_names[@]}"; do
+    mpath="${map_dir}${i}.map"
+    spath="${scen_dir}${i}.map.scen"
+
+    cmd="./test -run ${mpath} ${spath} > outputs/${i}.txt"
     echo $cmd
     eval $cmd
   done
-done
 
-echo "***** run competitor: tree *****"
-# run tree
-cd ./competitors/tree
+  cd -
+}
 
-for i in "${map_names[@]}"; do
-  mpath="${map_dir}${i}.map"
-  spath="${scen_dir}${i}.map.scen"
-
-  cmd="./test -run ${mpath} ${spath} > outputs/${i}.txt"
-  echo $cmd
-  eval $cmd
-done
-
-cd -
-
-echo "***** run competitor: focal *****"
-# run focal
-for i in "${map_names[@]}"; do
-  mpath="${map_dir}${i}.map"
-  spath="${scen_dir}${i}.map.scen"
-  for c in `seq 0 2 8`; do
-    cmd="./bin/focal ${mpath} ${spath} ${c}"
-    echo $cmd
-    eval $cmd
+run_focal() {
+  echo "----- run competitor: focal -----"
+  # run focal
+  for i in "${map_names[@]}"; do
+    mpath="${map_dir}${i}.map"
+    spath="${scen_dir}${i}.map.scen"
+    for c in `seq 0 2 8`; do
+      cmd="./bin/focal ${mpath} ${spath} ${c}"
+      echo $cmd
+      eval $cmd
+    done
   done
+}
+
+run() {
+  case "$1" in 
+    cpd) run_cpd ;;
+    tree) run_tree ;;
+    focal) run_focal ;;
+    all)
+      run_cpd
+      run_tree
+      run_focal;;
+    *) 
+      run_cpd
+      run_tree
+      run_focal ;;
+  esac
+}
+
+
+run $1
+if [ "$1" != "" ]
+then
+  shift
+fi
+while [ "$1" != "" ]; do
+  run $1
+  shift
 done
 
 echo "Done."
