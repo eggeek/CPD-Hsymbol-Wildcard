4b18d2d9c6843b4a1e74a55251f1b44a2ddeba1b
eggeek <eggeek.me@gmail.com>
2019-09-03 16:16:21 +1000

    add dijstra experiments

--- tests/test-preprocess.cpp
+++ tests/test-preprocess.cpp
@@ -1,7 +1,11 @@
 #include <vector>
 #include <iomanip>
 #include <fstream>
+#include <chrono>
+#include <time.h>
+#include <algorithm>
 #include "catch.hpp"
+#include "dijkstra.h"
 #include "preprocessing.h"
 #include "loader.h"
 #include "balanced_min_cut.h"
@@ -65,6 +69,61 @@ namespace TEST_PREPROCESS {
       LoadMap(mpath.c_str(), mapData, width, height);
       Parameters p{"DFS", "vanilla", output, 3, centroid};
       PreprocessCentroid(mapData, width, height, p);
+    }
+  }
+
+  TEST_CASE("dijkstra", "[.pre]") {
+    int repeat = 10;
+    int queries = 1000;
+    ifstream file(default_testcase_path + "dijkstra.in");
+    string output = "dijkstra-tcost.txt";
+    ofstream out(output);
+    srand(time(NULL));
+    string header = "map,i,source,s,h,w,e";
+    out << header << endl;
+    while (file >> mpath) {
+      LoadMap(mpath.c_str(), mapData, width, height);
+      Mapper mapper(mapData, width, height);
+      AdjGraph g(extract_graph(mapper));
+      vector<int> arr;
+      arr.resize(mapper.node_count());
+      for (int i=0; i<(int)arr.size(); i++) 
+        arr[i] = i;
+      std::random_shuffle(arr.begin(), arr.end());
+      queries = min(queries, mapper.node_count());
+      Dijkstra dij(g, mapper);
+
+      vector<unsigned short> allowed, inv_allowed;
+      string mapname = getMapName(mpath);
+
+      for (int i=0; i<repeat; i++) {
+        for (int j=0; j<queries; j++) {
+          auto stime = std::chrono::steady_clock::now();
+            dij.run(arr[j], 0);
+          auto etime = std::chrono::steady_clock::now();
+          double searchcost = std::chrono::duration_cast<std::chrono::nanoseconds>(etime - stime).count();
+
+          inv_allowed = vector<unsigned short>(dij.get_inv_allowed().begin(), dij.get_inv_allowed().end());
+          allowed = vector<unsigned short>(dij.get_allowed().begin(), dij.get_allowed().end());
+
+          stime = std::chrono::steady_clock::now();
+            H::encode(arr[j], allowed, mapper, 3);
+          etime = std::chrono::steady_clock::now();
+          double hcost = std::chrono::duration_cast<std::chrono::nanoseconds>(etime - stime).count();
+
+          stime = std::chrono::steady_clock::now();
+            SquareWildcard(mapper, mapper(arr[j])).computeMaxSquare(allowed);
+          etime = std::chrono::steady_clock::now();
+          double scost = std::chrono::duration_cast<std::chrono::nanoseconds>(etime - stime).count();
+
+          stime = std::chrono::steady_clock::now();
+            H::add_extr_inv_move(arr[j], inv_allowed, mapper);
+          etime = std::chrono::steady_clock::now();
+          double ecost = std::chrono::duration_cast<std::chrono::nanoseconds>(etime - stime).count();
+
+          out << mapname << "," << i << "," << arr[j] << "," << searchcost << "," << hcost << "," << scost << "," << ecost << endl;
+        }
+      }
     }
   }
 }
