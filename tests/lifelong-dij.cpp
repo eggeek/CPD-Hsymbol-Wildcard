a40498b1f25ef42cb8743be81fd6f97e5c5ce217
eggeek <eggeek.me@gmail.com>
2021-05-10 13:17:32 +1000

    Added: lifelong dij testing under dfs ordering

--- tests/lifelong-dij.cpp
+++ tests/lifelong-dij.cpp
@@ -89,6 +89,82 @@ namespace TEST_LIFELONG_DIJ {
     }
   }
 
+
+TEST_CASE("dfs-ll-dij") {
+  vector<string> argvs = 
+  {      
+    // "./maps/dao/den204d.map",
+    // "./tests/maps/arena.map",
+    // "./maps/brc000d.map",
+    "./maps/gppc/AcrosstheCape.map",
+  };
+  const long long INF = numeric_limits<int>::max();
+  for (auto& argv: argvs) {
+    string& mpath= argv;
+    LoadMap(mpath.c_str(), mapData, width, height);
+    Mapper mapper(mapData, width, height);
+    NodeOrdering order = compute_real_dfs_order(extract_graph(mapper));
+    mapper.reorder(order);
+    AdjGraph g(extract_graph(mapper));
+
+    Dijkstra dij(g, mapper);
+    LifeLongDijkstra lldij(g, mapper);
+
+    double lldij_cost = 0, dij_cost = 0, avg_prop = 0, avg_expan = 0;
+
+    vector<int> from_direction = vector<int>(g.node_count(), 16);
+    vector<int> dist = vector<int>(g.node_count(), 0);
+    vector<int> lldist = vector<int>(g.node_count(), 0);
+    lldij.set_from_direction(from_direction);
+
+    auto stime = std::chrono::steady_clock::now();
+    for (int i=0; i<10; i++) {
+      dij.run(i, 0);
+    }
+    auto etime = std::chrono::steady_clock::now();
+    double sampledij = std::chrono::duration_cast<std::chrono::nanoseconds>(etime - stime).count();
+    sampledij /= 10.0;
+
+    int num = min(g.node_count(), 10000);
+    for (int i=0; i<num; i++) {
+      auto stime = std::chrono::steady_clock::now();
+      dij.run(i, 0);
+      auto etime = std::chrono::steady_clock::now();
+      dij_cost += std::chrono::duration_cast<std::chrono::nanoseconds>(etime - stime).count();
+
+      stime = std::chrono::steady_clock::now();
+      long long delta = i == 0? INF: dij.get_dist()[i-1];
+      lldij.init(lldist, i, delta);
+      auto res = lldij.run(i, lldist);
+      etime = std::chrono::steady_clock::now();
+
+      avg_prop += res.first;
+      avg_expan += res.second;
+      double arun = std::chrono::duration_cast<std::chrono::nanoseconds>(etime - stime).count();
+
+      // Checking
+      dist = vector<int>(dij.get_dist());
+      lldist = vector<int>(lldij.get_dist());
+      build_from_direction(from_direction, dist, g);
+      validate_dist(dist, lldist, g);
+      validate_from_direction(from_direction, dist, lldij.get_from_direction(), lldist, g);
+
+      if (i % 10 == 0) {
+        cout << "avg prop: " << avg_prop / (double)(i+1)
+             << ", avg expan: " << avg_expan / (double)(i+1)
+             << ", speed up: " << dij_cost / lldij_cost << endl;
+      }
+    }
+    cout << "dij tcost: " << dij_cost << ", lldij tcost: " << lldij_cost 
+         << ", speed up: " << dij_cost / lldij_cost
+         << ", avg propagate: " << avg_prop / (double)num
+         << ", v: " << g.node_count()
+         << ", avg expan: " << avg_expan / (double)num << endl;
+    cout << "==============" << endl;
+
+  }
+}
+
   TEST_CASE("ll-dij") {
     vector<tuple<string, vector<int>>> argvs = {
       {"./maps/dao/den204d.map", {1493, 1550}},
@@ -147,13 +223,13 @@ namespace TEST_LIFELONG_DIJ {
         validate_dist(dist, lldist, g);
         validate_from_direction(from_direction, dist, lldij.get_from_direction(), lldist, g);
       }
-      cerr << "dij tcost: " << dij_cost << ", lldij tcost: " << lldij_cost 
+      cout << "dij tcost: " << dij_cost << ", lldij tcost: " << lldij_cost 
            << ", speed up: " << dij_cost / lldij_cost
            << ", #node: " << nodes.size()
            << ", avg propagate: " << avg_prop / (double)nodes.size()
            << ", v: " << g.node_count()
            << ", avg expan: " << avg_expan / (double)nodes.size() << endl;
-      cerr << "==============" << endl;
+      cout << "==============" << endl;
     }
   }
 
